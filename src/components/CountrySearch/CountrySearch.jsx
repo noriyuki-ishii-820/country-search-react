92efbf5f1b062c42c70d091f9c425bce7a6ab2c2
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-28 15:06:44 +1100

    basic search box done

--- src/components/CountrySearch/CountrySearch.jsx
+++ src/components/CountrySearch/CountrySearch.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from "./CountrySearch.module.css";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 export default function Country (){
+    const getSearch = e => {
+        e.preventDefault();
+        var input = document.getElementById("outlined-search").value
+
+        console.log(input)
+    }
     return (
         <div className={styles.container}>
-            This is the country page
+            <div className={styles.searchBox}>
+                <form onSubmit={getSearch} className={styles.searchForm}>
+                    <TextField className={styles.searchTextField} id="outlined-search" label="Type the name of a Country!" type="search" variant="outlined" />
+                    <Button variant="contained" color="primary" type="submit">
+                        Search
+                    </Button>
+                </form>
+            </div>
         </div>
        
     )
